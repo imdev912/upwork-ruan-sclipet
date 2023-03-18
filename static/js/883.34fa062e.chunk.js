9c4a56778a9926e75a0b64a34e4254fe52692df7
imdev912 <imdev912@gmail.com>
2023-03-19 03:41:58 +0530

    Updates

---
+++ static/js/883.34fa062e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkruan_sclipet=self.webpackChunkruan_sclipet||[]).push([[883],{1032:function(e,t,n){n.r(t);var u=n(5287),a=(n(9620),n(184));t.default=function(e){var t=e.link,n=(0,u.K)();return(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("a",{className:n?"pulsate pulsate-fwd":"pulsate",href:t,role:"button",children:e.text})})}},9620:function(){}}]);
+//# sourceMappingURL=883.34fa062e.chunk.js.map
